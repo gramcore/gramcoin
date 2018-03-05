b47dc8599e90bb9fcdaf76b27a5ca90fbd578ad2
GRAM CORE <36583097+gramcore@users.noreply.github.com>
2018-03-05 03:40:06 +0300

    Update qt source code

---
+++ src/qt/test/rpcnestedtests.h
@@ -0,0 +1,25 @@
+// Copyright (c) 2016 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef GRAM_QT_TEST_RPC_NESTED_TESTS_H
+#define GRAM_QT_TEST_RPC_NESTED_TESTS_H
+
+#include <QObject>
+#include <QTest>
+
+#include "txdb.h"
+#include "txmempool.h"
+
+class RPCNestedTests : public QObject
+{
+    Q_OBJECT
+
+    private Q_SLOTS:
+    void rpcNestedTests();
+
+private:
+    CCoinsViewDB *pcoinsdbview;
+};
+
+#endif // GRAM_QT_TEST_RPC_NESTED_TESTS_H
